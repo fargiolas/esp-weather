5dc9bbcefde37a719a69f2c72c99b2fe95bf2f0e
Filippo Argiolas <filippo.argiolas@gmail.com>
2023-01-02 12:09:42 +0100

    keep the wifi event handler around to handle disconnects
    Need to work on the wifi manager, the one from the examples lacks
    basic functionality like auto reconnection in case of disassociation.
    
    Just keep the event handler around and go to sleep on disconnection.

--- main/user_wifi.c
+++ main/user_wifi.c
@@ -54,7 +54,9 @@ static void event_handler(void* arg, esp_event_base_t event_base,
             s_retry_num++;
             ESP_LOGI(TAG, "retry to connect to the AP");
         } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            ESP_LOGI(TAG, "Stopping wifi and entering deep sleep for 60s");
+            esp_wifi_stop();
+            esp_deep_sleep(60 * 1000 * 1000);
         }
         ESP_LOGI(TAG,"connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
@@ -100,30 +102,19 @@ void wifi_init_sta(void)
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
 
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    /* wait until the connection is established (WIFI_CONNECTED_BIT) */
     EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
+                                           WIFI_CONNECTED_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
 
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
      * happened. */
     if (bits & WIFI_CONNECTED_BIT) {
         ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
                  WIFI_SSID, WIFI_PASS);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 WIFI_SSID, WIFI_PASS);
-        ESP_LOGI(TAG, "Stopping wifi and entering deep sleep for 60s");
-        esp_wifi_stop();
-        esp_deep_sleep(60 * 1000 * 1000);
     } else {
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
-
-    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
-    vEventGroupDelete(s_wifi_event_group);
 }
